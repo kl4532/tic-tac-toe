741b001b62e2c11a89ca56b44d51398914bf7362
Korneliusz Lukasiak <kl@webundsoehne.com>
2021-10-27 18:40:08 +0200

    implement game logic for two players

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {GameService} from "./components/services/game.service";
 
 export interface Field {
   id: number;
@@ -13,17 +14,40 @@ export interface Field {
 export class AppComponent implements OnInit{
   fields: Field[] = [];
   current = 0;
+
+  constructor(private gameService: GameService) {}
+
   ngOnInit(): void {
-    for(let i = 0; i<9; i++) {
-      const field: Field = {
-        id: i,
-        val: 0
-      }
-      this.fields?.push(field);
-    }
+    this.fields = this.gameService.init();
   }
 
   setValue(i: any) {
     this.fields[i].val = this.current;
+    this.checkGame(this.fields);
+  }
+
+  checkGame(fields: Field[]) {
+    const status = this.gameService.checkGameState(fields, this.current);
+    switch (status) {
+      case 1:
+        setTimeout(() => {
+          alert('X wins');
+          this.fields = this.gameService.init();
+        })
+        break;
+      case 2:
+        setTimeout(() => {
+          alert('O wins');
+          this.fields = this.gameService.init();
+        })
+        break;
+      case 3:
+        setTimeout(() => {
+          alert('DRAW!');
+          this.fields = this.gameService.init();
+        })
+        break;
+      default:
+    }
   }
 }
---
+++ src/app/components/services/game.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {Field} from "../../app.component";
+import {BehaviorSubject} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+  constructor() { }
+
+  init(): Field[] {
+    let fields = []
+    for(let i = 0; i<9; i++) {
+      const field: Field = {
+        id: i,
+        val: 0
+      }
+      fields.push(field);
+    }
+    return fields;
+  }
+
+  checkGameState(fields: Field[], lastMark: number): number {
+    const isDraw = fields.find(f => f.val === 0);
+
+    if(!isDraw) {
+      return 3;
+    }
+
+    if(
+      fields[0].val === fields[1].val && fields[0].val === fields[2].val && fields[0].val !== 0 ||
+      fields[3].val === fields[4].val && fields[3].val === fields[5].val && fields[3].val !== 0 ||
+      fields[6].val === fields[7].val && fields[6].val === fields[8].val && fields[6].val !== 0 ||
+
+      fields[0].val === fields[3].val && fields[0].val === fields[6].val && fields[0].val !== 0 ||
+      fields[1].val === fields[4].val && fields[1].val === fields[7].val && fields[1].val !== 0 ||
+      fields[2].val === fields[5].val && fields[2].val === fields[8].val && fields[2].val !== 0 ||
+
+      fields[0].val === fields[4].val && fields[0].val === fields[8].val && fields[0].val !== 0 ||
+      fields[2].val === fields[4].val && fields[2].val === fields[6].val && fields[2].val !== 0
+    ) {
+      return lastMark
+    }
+    return 0;
+  }
+
+}
