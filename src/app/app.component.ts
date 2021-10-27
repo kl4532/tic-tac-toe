9586aa097a147abb6bc47577d63a562444c21c98
Korneliusz Lukasiak <kl@webundsoehne.com>
2021-10-27 16:51:55 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+export interface Field {
+  id: number;
+  val: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tic-tac-toe';
+export class AppComponent implements OnInit{
+  fields: Field[] = [];
+  current = 0;
+  ngOnInit(): void {
+    for(let i = 0; i<9; i++) {
+      const field: Field = {
+        id: i,
+        val: 0
+      }
+      this.fields?.push(field);
+    }
+  }
+
+  setValue(i: any) {
+    this.fields[i].val = this.current;
+  }
 }
---
+++ src/app/components/field/field.component.ts
@@ -0,0 +1,47 @@
+import {Component, EventEmitter, forwardRef, Input, OnInit, Output} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+
+@Component({
+  selector: 'app-field',
+  templateUrl: './field.component.html',
+  styleUrls: ['./field.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => FieldComponent),
+      multi: true
+    }
+  ]
+})
+export class FieldComponent implements OnInit, ControlValueAccessor {
+  @Output() changeMark = new EventEmitter<number>();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onChange: any = () => {}
+  onTouch: any = () => {}
+  val: number | undefined
+  set value(val: number){  // this value is updated by programmatic changes if( val !== undefined && this.val !== val){
+    if(!this.val) {
+      this.val = val
+      this.onChange(val)
+      this.onTouch(val)
+      this.changeMark.emit(val);
+    }
+  }
+  // this method sets the value programmatically
+  writeValue(value: number){
+      this.value = value
+  }
+  // upon UI element value changes, this method gets triggered
+  registerOnChange(fn: any){
+    this.onChange = fn
+  }
+  // upon touching the element, this method gets triggered
+  registerOnTouched(fn: any){
+    this.onTouch = fn
+  }
+
+}
